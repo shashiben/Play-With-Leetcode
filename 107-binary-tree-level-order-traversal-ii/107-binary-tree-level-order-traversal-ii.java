f948cd9a4d2bc4b9d5a5efa267f1a7e8ab4d406a
Shashi Kumar <kumarshashi5294@gmail.com>
2022-09-08 19:04:17 +0530

    Time: 8 ms (5.16%), Space: 44 MB (18.42%) - LeetHub

---
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Map<Integer,List<Integer>> map=new HashMap<>();
+        addLevelWise(root,map,0);
+        System.out.println(map);
+        List<List<Integer>> result=new ArrayList<>();
+        for (List<Integer> list :map.values()){
+            result.add(list);
+        } 
+        Collections.reverse(result);
+        return result;
+    }
+    public void addLevelWise(TreeNode root,Map<Integer,List<Integer>> map,int level){
+        if(root==null){
+            return;
+        }
+        List<Integer> temp=new ArrayList<>();
+        List<Integer> def=map.getOrDefault(level,temp);
+        def.add(root.val);
+        map.put(level,def);
+        addLevelWise(root.left,map,level+1);
+        addLevelWise(root.right,map,level+1);
+    }
+}
