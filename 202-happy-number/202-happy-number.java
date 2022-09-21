6aaea88eb32e2aadec1f935b397ed381181e68c5
Shashi Kumar <kumarshashi5294@gmail.com>
2022-09-21 13:36:44 +0530

    Time: 2 ms (68.14%), Space: 40.6 MB (81.89%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,21 +1,29 @@
 class Solution {
     public boolean isHappy(int n) {
-        if(n==1){
-            return true;
-        }
-        int temp=0;
-        HashSet<Integer> hashSet=new HashSet<>();
-        while(!hashSet.contains(n)){
-            n=doSquare(n);
-            if(n==1){
-                return true;
-            }
-            hashSet.add(temp);
-            temp=n;
-            System.out.println(hashSet+","+n);
-        }
-        return false;
+        int slow=n;
+        int fast=n;
+        do{
+            slow=doSquare(slow);
+            fast=doSquare(doSquare(fast));
+        }while(slow!=fast);
+        return slow==1;
     }
+    //     if(n==1){
+    //         return true;
+    //     }
+    //     int temp=0;
+    //     HashSet<Integer> hashSet=new HashSet<>();
+    //     while(!hashSet.contains(n)){
+    //         n=doSquare(n);
+    //         if(n==1){
+    //             return true;
+    //         }
+    //         hashSet.add(temp);
+    //         temp=n;
+    //         System.out.println(hashSet+","+n);
+    //     }
+    //     return false;
+    // }
     public int doSquare(int n){
         int result=0;
         if(n<10){
